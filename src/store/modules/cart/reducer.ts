d1b166eec4def7001d1caab55f4c7c97dbc44484
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-10-20 08:33:22 -0300

    redux configuration and store

---
+++ src/store/modules/cart/reducer.ts
@@ -0,0 +1,12 @@
+import { Reducer } from "redux";
+import { ICartState } from "./types";
+
+const INITAL_STATE: ICartState = {
+  items:[]
+}
+
+const cart: Reducer<ICartState> = () => {
+  return INITAL_STATE
+}
+
+export default cart;
